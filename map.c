2df3ddc05c5753d793febcac6cca49992cfe5974
Carmelo Oliveira De Brito Lira Filho <carmoliv@c2r1s3.42porto.com>
2026-01-06 23:06:18 +0000

    Update

--- map.c
+++ map.c
@@ -1,73 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: carmoliv <carmoliv@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/06 22:45:22 by carmoliv          #+#    #+#             */
+/*   Updated: 2026/01/06 23:05:46 by carmoliv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "so_long.h"
 
-static void trim_line(char *line)
+static	void	trim_line(char *line)
 {
-    int len;
+	int	len;
 
-    len = so_strlen(line);
-    while (len > 0 && (line[len - 1] == '\n' || line[len - 1] == '\r'))
-    {
-        line[len - 1] = '\0';
-        len--;
-    }
+	len = so_strlen(line);
+	while (len > 0 && (line[len - 1] == '\n' || line[len - 1] == '\r'))
+	{
+		line[len - 1] = '\0';
+		len--;
+	}
 }
 
-static int  get_map_height(const char *file)
+static	int	get_map_height(const char *file)
 {
-    int fd;
-    int height;
-    char *line;
+	int		fd;
+	int		height;
+	char	*line;
 
-    fd = open(file, O_RDONLY);
-    if (fd < 0)
-        return (-1);
-    height = 0;
-    while ((line = get_next_line(fd)))
-    {
-        free(line);
-        height++;
-    }
-    close(fd);
-    return (height);
+	fd = open(file, O_RDONLY);
+	if (fd < 0)
+		return (-1);
+	height = 0;
+	while ((line = get_next_line(fd)))
+	{
+		free(line);
+		height++;
+	}
+	close(fd);
+	return (height);
 }
 
-static char **fill_map(const char *file, int height)
+static	char	**fill_map(const char *file, int height)
 {
-    int fd;
-    int i;
-    char *line;
-    char **map;
+	int		fd;
+	int		i;
+	char	*line;
+	char	**map;
 
-    fd = open(file, O_RDONLY);
-    if(fd < 0)
-        return (NULL);
-    map = malloc(sizeof(char *) * (height + 1));
-    if(!map)
-        return (NULL);
-    i = 0;
-    while((line = get_next_line(fd)))
-    {
-        trim_line(line);
-        map[i++] = line;
-    }
-    map[i] = NULL;
-    close(fd);
-    return(map);
+	fd = open(file, O_RDONLY);
+	if (fd < 0)
+		return (NULL);
+	map = malloc(sizeof(char *) * (height + 1));
+	if (!map)
+		return (NULL);
+	i = 0;
+	while ((line = get_next_line(fd)))
+	{
+		trim_line(line);
+		map[i++] = line;
+	}
+	map[i] = NULL;
+	close(fd);
+	return (map);
 }
 
-char **read_map(const char *file, t_game *game)
+char	**read_map(const char *file, t_game *game)
 {
-    char **map;
+	char	**map;
 
-    game->height = get_map_height(file);
-    if (game->height <= 0)
-        return (NULL);
-    map = fill_map(file, game->height);
-    if (!map)
-        return (NULL);
-    if (map[0])
-        game->width = so_strlen(map[0]);
-    else
-        game->width = 0;
-    return (map);
+	game->height = get_map_height(file);
+	if (game->height <= 0)
+		return (NULL);
+	map = fill_map(file, game->height);
+	if (!map)
+		return (NULL);
+	if (map[0])
+		game->width = so_strlen(map[0]);
+	else
+		game->width = 0;
+	return (map);
 }
